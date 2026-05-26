e090e583818f8ee21b1cf0e1b7f5652b5ba88e9e
Anirban Mondal <anirbanmondal.reach@gmail.com>
2026-05-26 22:12:45 +0530

    Allow backend CORS for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_ORIGIN ?? "http://localhost:3000"
+    origin: "*"
   });
   app.useGlobalPipes(
     new ValidationPipe({
